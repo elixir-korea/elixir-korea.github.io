75987366f43cd7806596040c7865084801c0c09e
Jinkyou Son <nallwhy@gmail.com>
2020-01-04 11:13:33 +0900

    Update serum.exs

--- serum.exs
+++ serum.exs
@@ -1,10 +1,11 @@
 %{
-  site_name: "Elixir Korea",
-  site_description: "한국 엘릭서 사용자 모임",
-  date_format: "{WDfull}, {D} {Mshort} {YYYY}",
+  site_name: "Elixir Korea Blog",
+  site_description: "한국 엘릭서 사용자 모임 블로그",
+  author: "Elixir Korea",
+  author_email: "-",
   base_url: "/",
-  author: "John Doe",
-  author_email: "john.doe@example.com",
+  server_root: "https://elixir-korea.github.io/",
+  date_format: "{WDfull}, {D} {Mshort} {YYYY}",
   plugins: [
     Serum.Plugins.TableOfContents,
     {Serum.Plugins.LiveReloader, only: :dev}
